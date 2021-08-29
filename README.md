9f4e87824dbe6552de38392af7beb5ee5700197c
yuva <yuva.phalle@gmail.com>
2021-08-29 14:56:46 +0200

    Inital Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# A bot helps you not to forget clean the kitchen!
+
+Based on [pyTelegramBotAPI](https://github.com/eternnoir/pyTelegramBotAPI#pytelegrambotapi)
+
+
+# Dependicies: 
+
+* pyTelegramBotAPI
+
+* requests
+
+* PySocks
+
+* gunicorn
+
+* urllib3
+
+* preprocessing
+
+* datetime 
+
+
+To run bot set the environment variables:
+
+```bash
+export TOKEN="<access_token>"
+export CHAT_ID="<chat_id>"
+```
+
---
+++ src/botmessages.py
@@ -0,0 +1,37 @@
+# - coding: utf-8 -
+
+
+start = '''Hi FLowers !\n'''
+
+hlp = '''Good day!
+
+/help - show this help 
+/links - useful links
+/faq_en - answers for common questions
+
+For the kitchen cleaning schedule check pinned message and #cleaning hashtag
+
+'''
+
+links = '''Полезные ссылки: 
+sit.org -- "SIT"
+'''
+
+
+faq_en = '''There will be a FAQ
+
+'''
+
+clean_hashtag = '#cleaning'
+
+clean_headers = [
+                        'Cmon slow hands, Clean the Dirty Kitchen!\n',
+                        'Dont Be Lazy and start cleaning! \n',
+                        'Clean the Kitchen, if you are not an asshole \n'
+
+                    ]
+
+clean_body = '{}:\t{} and {}\n'
+
+
+
